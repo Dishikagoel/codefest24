b265c0609cbb4b2b470eb3bf6a5f47c0fff34f66
Mukul Periwal <mukulperiwal@n3-61-135.dhcp.drexel.edu>
2024-04-20 11:23:04 -0400

    App update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 
 function App() {
   return (
+      <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <p className="h1 text">
+          Hello, world!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link to="/"> Begin! </Link>
+        {/*<Routes>*/}
+        {/*  <Route path"/" element={<App />} />*/}
+          {/*<Route path="/index" element={<}*/}
+        {/*</Routes>*/}
       </header>
     </div>
+      </Router>
   );
 }
 
